21dc5018c79cda7bd7b1f320cd6956ae7841a166
MSDoshev <mihaildo@outlook.com>
2024-02-16 14:29:42 +0200

    Add quiz component
    - created the quiz component
    - added basic logic for question iteration

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
-function App() {}
+import Header from "./components/Header/Header";
+import Quiz from "./components/Quiz/Quiz";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+    </>
+  );
+}
 
 export default App;
